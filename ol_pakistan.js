9e452f2253f73c54f6dc5fc504f1ef20df88ef0c
usamarehan557 <usamarehan557@gmail.com>
2022-01-03 09:27:29 +0500

    tried to integrate wfs but failed, probably last commit

--- ol_pakistan.js
+++ ol_pakistan.js
@@ -1,6 +1,4 @@
 var pureCoverage = false
-// if this is just a coverage or a group of them, disable a few items,
-// and default to jpeg format
 var format = 'image/png'
 var bounds = [
   6779294.310947161,
@@ -8,17 +6,29 @@ var bounds = [
   8665451.873011257,
   4452637.588751792,
 ]
-
 if (pureCoverage) {
   format = 'image/jpeg'
 }
+var map;
+var dark_flag = 1;
+var osmBasemap;
+var mousePositionControl;
+var worldTopo;
+var worldImagery;
+
+function callDefaultData() {
+  create_MapObject();
+  getXml();
+  add_osm();
+}
 
 // ...................................................getting XML Data
-function getlayers() {
+function getXml() {
   var xhttp = new XMLHttpRequest()
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      myFunction(this)
+      myFunction(this);
+      turn_on_default_layer(0);
     }
   }
   xhttp.open('GET', 'note.xml', true)
@@ -31,222 +41,252 @@ function myFunction(xml) {
   // var y = x.childNodes[0];
   // var layername = y.nodeValue;
   // document.getElementById("demo").innerHTML = layername;
-  var x, i, txt, xmlDoc
-  xmlDoc = xml.responseXML
-  txt = ''
-  x = xmlDoc.getElementsByTagName('title')
+  var i, xmlDoc;
+
+  var x, i, txt;
+  xmlDoc = xml.responseXML;
+  txt = '';
+  x = xmlDoc.getElementsByTagName('title');
   for (i = 0; i < x.length; i++) {
     txt +=
-      `<input type="checkbox" id="html" onchange="switcher(this.value)" name="abc" value="` +
-      x[i].childNodes[0].nodeValue +
+      `<input type="checkbox" id="`+ i +`s" onchange="layer_switcher(this.value)" name="abc" value="` +
+      i +
+      // x[i].childNodes[0].nodeValue +
       `">&nbsp` +
       x[i].childNodes[0].nodeValue +
-      `<br/>`
+      `<br/>`;
+      n = i;
+      z = x[i].childNodes[0].nodeValue;
+      popper(z, i);
+      function popper(a, b) {
+        custom_AddLayers(a, b);
+      }
   }
-  document.getElementById('demo').innerHTML = txt
+  document.getElementById('demo').innerHTML = txt;
   // document.getElementById("demo").innerHTML =
   // xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
 }
 
-// ...................................................default image layer
-var default_layer = 'nyc:pakistanPAK_adm0_3857';
 
+// ...................................................Switching image layers old
+function layer_switcher(layer_id) {
 
-// ...................................................Switching image layers
-function switcher(layer) {
-  console.log("layer recieved = ", layer);
-  let search_result = default_layer.search(layer);
-  console.log("search result = ", search_result);
-
-  if (search_result == -1) {
-
-    changed_layer = default_layer.concat(`,`, layer);
-    default_layer = changed_layer;
-
-  } else if (search_result > -1) {
-    main_layer = 'nyc:pakistanPAK_adm0_3857';
-    default_layer = main_layer;
-
+  if (window["geoserved" + layer_id].getProperties().visible == true) {
+    window["geoserved" + layer_id].setVisible(false);
+  } else {
+    window["geoserved" + layer_id].setVisible(true);
   }
-  console.log("default layer = ", default_layer);
-  var params = geoserved.getSource().getParams()
-  params.LAYERS = default_layer
-  geoserved.getSource().updateParams(params)
-  console.log(params);
+  // var momo = window["geoserved" + layer_id].getProperties().visible;
+  // // var mimi = momo.visible;
+  // console.log(momo);
 }
-
-
-function tester() {
-  let text = "Mr. nyc:Blue,nyc:Red has a blue house"
-  let position = text.search("nyc:Red");
-  console.log(position);
-}
-
-// ...................................................mouse hover
-var mousePositionControl = new ol.control.MousePosition({
-  className: 'custom-mouse-position',
-  target: document.getElementById('location'),
-  coordinateFormat: ol.coordinate.createStringXY(5),
-  undefinedHTML: '&nbsp;',
-})
-
 
 // ...................................................showing image layer
-
-// function layerAdder() {
-  var geoserved = new ol.layer.Image({
+function custom_AddLayers(clayer, id) {
+  // var geoserved = [];
+  window["geoserved" + id] = new ol.layer.Image({
+    visible: true,
+    zIndex: 1,
     source: new ol.source.ImageWMS({
       ratio: 1,
       url: 'http://localhost:8081/geoserver/nyc/wms',
       params: {
+        ID: id,
         FORMAT: format,
         VERSION: '1.1.1',
         STYLES: '',
-        LAYERS: default_layer,
+        LAYERS: clayer,
         exceptions: 'application/vnd.ogc.se_inimage',
       },
     }),
   })
-// }
-
-
-
-function callDefaultData() {
-  getlayers();
-  create_MapObject();
-  custom_AddMaps();
-  custom_AddLayers();
+  map.addLayer(window["geoserved" + id]);
+  window["geoserved" + id].setOpacity(0.5);
+  window["geoserved" + id].setVisible(false);
+  // var mparams = map.getLayers();
+  // var params = window["geoserved" + id].getSource().getParams()
+  // console.log(mparams);
+  // console.log(params);
 }
+
+// ...................................................default basemap adder
+function add_osm() {
+  osmBasemap = new ol.layer.Tile({
+    source: new ol.source.OSM(),
+  })
+  map.addLayer(osmBasemap);
+}
+
+function turn_on_default_layer(id) {
+  // document.getElementById("flexSwitchCheckDefault").click();
+  document.getElementById("0s").click();
+  
+}
+
+// mapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmap
+
 
 function create_MapObject() {
-// ...................................................projection
-var projection = new ol.proj.Projection({
-  code: 'EPSG:3857',
-  units: 'm',
-  global: true,
-})
-// ...................................................map
-var map = new ol.Map({
-  controls: ol.control
-    .defaults({
-      attribution: false,
+  // mouse hover
+  mousePositionControl = new ol.control.MousePosition({
+    className: 'custom-mouse-position',
+    target: document.getElementById('location'),
+    coordinateFormat: ol.coordinate.createStringXY(5),
+    undefinedHTML: '&nbsp;',
+  })
+  // projection
+  var projection = new ol.proj.Projection({
+    code: 'EPSG:3857',
+    units: 'm',
+    global: true,
+  })
+  // map
+  map = new ol.Map({
+    controls: ol.control
+      .defaults({
+        attribution: false,
+      })
+      .extend([mousePositionControl]),
+    target: 'map',
+    // layers: [worldImagery, basemap, worldTopo, geoserved],
+    layers: [],
+    view: new ol.View({
+      projection: projection,
+    }),
+  })
+
+// world imagery basemap
+  worldImagery = new ol.layer.Tile({ 
+    visible: false,
+      source: new ol.source.XYZ({
+        url:
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}/',
+        maxZoom: 19,
+      }),
     })
-    .extend([mousePositionControl]),
-  target: 'map',
-  // layers: [worldImagery, basemap, worldTopo, geoserved],
-  layers: [],
-  view: new ol.View({
-    projection: projection,
-  }),
-})
-}
+    map.addLayer(worldImagery);
+    // worldImagery.setVisible(false);
+    // var params = worldImagery.getProperties().visible;
+    // console.log(params);
 
-function custom_AddLayers() {
-
-}
-
-function custom_AddMaps() {
-
-
-// ...................................................satellite image basemap
-var worldImagery = new ol.layer.Tile({
-  visible: true,
-  source: new ol.source.XYZ({
-    url:
-      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-    maxZoom: 19,
-  }),
-})
-map.addLayer(worldImagery);
-
-// ...................................................satellite image Topography basemap
-var worldTopo = new ol.layer.Tile({
+  // world topographic basemap
+  worldTopo = new ol.layer.Tile({
   visible: false,
-  source: new ol.source.XYZ({
-    url:
-      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
-    maxZoom: 19,
-  }),
-})
-map.addLayer(worldTopo);
-// ...................................................Open layers basemap
-var basemap = new ol.layer.Tile({
-  source: new ol.source.OSM(),
-})
-map.addLayer(basemap);
+    source: new ol.source.XYZ({
+      url:
+      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}/',
+      maxZoom: 19,
+    }),
+  })
+  map.addLayer(worldTopo);
+
+  // var vector = new VectorLayer({
+  //   source: new VectorSource({
+  //     url: 'test.geojson',
+  //     format: new GeoJSON(),
+  //   }),
+  // })
+  // map.addLayer(vector);
+
+
+
+  // resolution changer
+  map.getView().on('change:resolution', function (evt) {
+    var resolution = evt.target.get('resolution')
+    var units = map.getView().getProjection().getUnits()
+    var dpi = 25.4 / 0.28
+    var mpu = ol.proj.METERS_PER_UNIT[units]
+    var scale = resolution * mpu * 39.37 * dpi
+    if (scale >= 9500 && scale <= 950000) {
+      scale = Math.round(scale / 1000) + 'K'
+    } else if (scale >= 950000) {
+      scale = Math.round(scale / 1000000) + 'M'
+    } else {
+      scale = Math.round(scale)
+    }
+    document.getElementById('scale').innerHTML = 'Scale = 1 : ' + scale
+  })
+  map.getView().fit(bounds, map.getSize());
+
+  map.on('singleclick', function(evt) {
+    // document.getElementById('nodelist').innerHTML = "Loading... please wait...";
+    // var view = map.getView();
+    // var viewResolution = view.getResolution();
+    // var source = window["geoserved" + iid].get('visible') ? window["geoserved" + iid].getSource() : window["geoserved" + iid].getSource();
+    // var url = source.getGetFeatureInfoUrl(
+    //   evt.coordinate, viewResolution, view.getProjection(),
+    //   {'INFO_FORMAT': 'text/html', 'FEATURE_COUNT': 50});
+    // if (url) {
+    //   document.getElementById('nodelist').innerHTML = '<iframe seamless src="' + url + '"></iframe>';
+    map.forEachFeatureAtPixel(evt.pixel, function (feature, layer) {
+      console.log('clicked', layer);
+    });
+    
+    // }
+  });
+
+  // map.on('click', function(e) {
+  //   map.forEachLayerAtPixel(e.pixel, function(layer) {
+  //     // log the LAYERS param of the WMS source
+  //     console.log(layer.getSource().getParams().LAYERS);
+  //   });
+  // });
+
+
 }
-// // ...................................................projection
-// var projection = new ol.proj.Projection({
-//   code: 'EPSG:3857',
-//   units: 'm',
-//   global: true,
-// })
-// // ...................................................map
-// var map = new ol.Map({
-//   controls: ol.control
-//     .defaults({
-//       attribution: false,
-//     })
-//     .extend([mousePositionControl]),
-//   target: 'map',
-//   // layers: [worldImagery, basemap, worldTopo, geoserved],
-//   layers: [],
-//   view: new ol.View({
-//     projection: projection,
-//   }),
-// })
+// let selected = null;
+// const status = document.getElementById('status');
+
+// map.on('singleclick', function (e) {
+//   if (selected !== null) {
+//     selected.setStyle(undefined);
+//     selected = null;
+//   }
+
+//   map.forEachFeatureAtPixel(e.pixel, function (f) {
+//     selected = f;
+//     f.setStyle(highlightStyle);
+//     return true;
+//   });
+
+//   if (selected) {
+//     status.innerHTML = '&nbsp;Hovering: ' + selected.get('name');
+//   } else {
+//     status.innerHTML = '&nbsp;';
+//   }
+// });
 
 
 
-// ...................................................tile layer opacity
+// mapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmap
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  geoserver.setOpacity(this.value)
-}
-
-
-// ...................................................resolution changer
-map.getView().on('change:resolution', function (evt) {
-  var resolution = evt.target.get('resolution')
-  var units = map.getView().getProjection().getUnits()
-  var dpi = 25.4 / 0.28
-  var mpu = ol.proj.METERS_PER_UNIT[units]
-  var scale = resolution * mpu * 39.37 * dpi
-  if (scale >= 9500 && scale <= 950000) {
-    scale = Math.round(scale / 1000) + 'K'
-  } else if (scale >= 950000) {
-    scale = Math.round(scale / 1000000) + 'M'
-  } else {
-    scale = Math.round(scale)
-  }
-  document.getElementById('scale').innerHTML = 'Scale = 1 : ' + scale
-})
-map.getView().fit(bounds, map.getSize())
-
-
-// ...................................................basemap changer
+// basemap changer
 function worldImg() {
-  basemap.set('visible', false)
-  worldImagery.set('visible', true)
+  osmBasemap.set('visible', false);
+  worldImagery.set('visible', true);
   worldTopo.set('visible', false)
 }
 function osmap() {
-  basemap.set('visible', true)
+  osmBasemap.set('visible', true)
   worldImagery.set('visible', false)
   worldTopo.set('visible', false)
 }
 function topomap() {
-  basemap.set('visible', false)
+  osmBasemap.set('visible', false)
   worldImagery.set('visible', false)
   worldTopo.set('visible', true)
 }
 
-var dark_flag = 1;
+// opacity changer
+var slider = document.getElementById("myRange");
+// var output = document.getElementById("demo");
+// output.innerHTML = slider.value;
+slider.oninput = function() {
+  geoserved.setOpacity(this.value)
+}
 
+
+//  dark map function
 function darkmap() {
   //change map color
   if (dark_flag == 1) {
@@ -261,6 +301,11 @@ function darkmap() {
   // });
 }
 
+function tester(num) {
+  var params = window["geoserved" + num].getSource().getParams()
+  console.log(params);
+  window["geoserved" + num].setOpacity(0.5);
+}
 
 // var xhttp = new XMLHttpRequest();
 // xhttp.onreadystatechange = function() {
@@ -288,4 +333,23 @@ function darkmap() {
 //     console.log(txt);
 //     // document.getElementById("demo").innerHTML =
 //     // xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+// }
+
+// ...................................................custom basemap adder
+// function custom_AddBaseMap(mapUrl, num) {
+// var m_url = `"` + mapUrl + `"`;
+//  window["basemap" + num] = new ol.layer.Tile({
+//   // var basemap1 = new ol.layer.Tile({ 
+//   visible: true,
+//     source: new ol.source.XYZ({
+//       url:
+//       mapUrl,
+//       maxZoom: 19,
+//     }),
+//   })
+//   console.log("topo called", mapUrl);
+//   map.addLayer(window["basemap" + num]);
+//   window["basemap" + num].setVisible(false);
+//   var params = window["basemap" + num].getProperties().visible;
+//   console.log(params)
 // }
